ed20c80e4da484274b7fcceada09c9c39aa4c3ba
HM\FRBUL <fredrik.bullsimonsen@hm.com>
2020-12-08 14:29:51 +0100

    Fixed first web page

--- src/main/java/com/example/noticeboard/AdvertRepository.java
+++ src/main/java/com/example/noticeboard/AdvertRepository.java
@@ -10,7 +10,6 @@ public ArrayList<Advert>advertList= new ArrayList<>();
         advertList.add(new Advert(1,"CarSeat for baby", "very nice car seat for baby up to 2 years", 299,"",0,0 ));
         advertList.add(new Advert(2,"Fjällstuga i Åre uthyres", "Jättemysig stuga med 2 rok nära lift", 3000,"",0,0 ));
         advertList.add(new Advert(3,"Matbord", "Använt men i gott skick! Bortskänkes, först till kvarn", 0,"",0,0 ));
-
     }
 }
 
