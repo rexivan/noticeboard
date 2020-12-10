7c1b51932c0c8a02f61a354ac8939d066c3ac888
HM\STGAB <stanley.gabari@hm.com>
2020-12-10 08:49:32 +0100

    Fixed login and sign up templates.

--- src/main/java/com/example/noticeboard/NoticeboardController.java
+++ src/main/java/com/example/noticeboard/NoticeboardController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -50,5 +47,16 @@ public class NoticeboardController {
             System.out.println("Got post, NULL!");
         advertRepository.save(advert);
         return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/signUp")
+    public String signUp(){
+        return "signUp";
+
     }
 }
